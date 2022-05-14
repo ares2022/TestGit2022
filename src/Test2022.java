f9188a9e991ea742b8c635dfd2cb82ac7d8a7050
ares2022 <ares2022@ares.com>
2022-05-14 15:08:38 +0800

    v1.02

--- src/Test2022.java
+++ src/Test2022.java
@@ -2,5 +2,6 @@ public class Test2022 {
     public static void main(String[] args) {
         System.out.println("go");
         System.out.println("666");
+        System.out.println("123");
     }
 }
